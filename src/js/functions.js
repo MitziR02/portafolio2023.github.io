b5d7db6c47b01250575cb28f2716dc71d3758c88
Mitzi Rios PC <mitzi.rios02@gmail.com>
2023-10-30 13:05:50 -0600

    Se arregló funtions.js agregando localStorage

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,23 +1,34 @@
-document.getElementById('toggle').addEventListener('change', function() {
-    var icon=document.getElementById("animation-On");
-    var layer=document.getElementById("layer");
-    if (this.checked) {
-        layer.style.opacity = "1";
+document.getElementById('toggle').addEventListener('change', toggleAnimation);
 
+function toggleAnimation() {
+    var icon = document.getElementById("animation-On");
+    var layer = document.getElementById("layer");
+    let isChecked = document.getElementById('toggle').checked;
+
+    if (isChecked) {
+        layer.style.opacity = "1";
         icon.innerHTML='<iconify-icon icon="material-symbols:toggle-off-outline"></iconify-icon>';
     } else {
         layer.style.opacity = "0.8";
-
         icon.innerHTML='<iconify-icon icon="material-symbols:toggle-on"></iconify-icon>';
     }
-});
+    
+    localStorage.setItem("animationState", isChecked ? "enabled" : "disabled");
+}
 
-document.getElementById('checkDark').addEventListener('change', function() {
-    var icon=document.getElementById("darkMode");
+// DarkMode
+
+document.getElementById('checkDark').addEventListener('change', toggleDarkMode);
+
+function toggleDarkMode() {
+    var icon = document.getElementById("darkMode");
     let root = document.documentElement;
     let imgElement = document.getElementById('imgLogo');
+    let isChecked = document.getElementById('checkDark').checked;
 
-    if (this.checked) {
+    localStorage.setItem("darkMode", isChecked ? "enabled" : "disabled");
+
+    if (isChecked) {
 
         root.style.setProperty('--grey', '#fffaf5');
         root.style.setProperty('--white', '#30323c');
@@ -27,6 +38,7 @@ document.getElementById('checkDark').addEventListener('change', function() {
         imgElement.src = "./src/assets/LogoLight.png";
 
         icon.innerHTML='<iconify-icon icon="material-symbols:sunny-outline-rounded"></iconify-icon>';
+
     } else {
 
         root.style.setProperty('--grey', '#30323c');
@@ -38,4 +50,31 @@ document.getElementById('checkDark').addEventListener('change', function() {
 
         icon.innerHTML='<iconify-icon icon="solar:moon-bold"></iconify-icon>';
     }
-});
+}
+
+window.onload = function() {
+
+    const currentAnimationSetting = localStorage.getItem("animationState");
+    const toggleBtn = document.getElementById("toggle");
+
+    if (currentAnimationSetting === "enabled") {
+        toggleBtn.checked = true;
+    } else if (currentAnimationSetting === "disabled") {
+        toggleBtn.checked = false;
+    }
+
+    toggleAnimation(); 
+
+    // DARK MODE
+
+    const currentDarkModeSetting = localStorage.getItem("darkMode");
+    const checkbox = document.getElementById("checkDark");
+
+    if (currentDarkModeSetting === "enabled") {
+        checkbox.checked = true;
+    } else if (currentDarkModeSetting === "disabled") {
+        checkbox.checked = false;
+    }
+
+    toggleDarkMode();
+};
